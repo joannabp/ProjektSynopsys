c08fb089e2e66e46e73e1b6acb1f775e37514377
joannabp <joannabaranska@yahoo.com>
2017-05-15 19:01:22 +0200

    driver z filtrowaniem

--- driv_script.m
+++ driv_script.m
@@ -12,48 +12,27 @@ end
 j=1;
 for i=1:50
         if pam4_vect(1,i)==0
-            out(j)=-280;
-            out(j+1)=-285;
-            out(j+2)=-299;
-            %out(j+3:j+31)=-300;
-            out(j+3:j+28)=-300;
-            out(j+29)=-299;
-            out(j+30)=-290;
-            out(j+31)=-288;
+            
+            out(j:j+31)=-300;
+           
             j=j+32;
         elseif  pam4_vect(1,i)==1
-            out(j)=-80;
-            out(j+1)=-85;
-            out(j+2)=-99;
-            out(j+3:j+28)=-100;
-            out(j+29)=-99;
-            out(j+30)=-90;
-            out(j+31)=-88;
+            out(j:j+31)=-100;
             j=j+32;
         elseif  pam4_vect(1,i)==10
-             out(j)=80;
-            out(j+1)=85;
-            out(j+2)=99;
-            out(j+3:j+28)=100;
-            out(j+29)=99;
-            out(j+30)=90;
-            out(j+31)=88;
+
+            out(j:j+31)=100;
             j=j+32;
         else 
-            out(j)=280;
-            out(j+1)=285;
-            out(j+2)=299;
-            out(j+3:j+28)=300;
-            out(j+29)=299;
-            out(j+30)=290;
-            out(j+31)=288;
+
+            out(j:j+31)=300;
             j=j+32;
         end
 end
 
-plot(1:1600,out);
+%plot(1:1600,out);
 alpha = 0.45;
 exponentialMA_data = filter(alpha, [1 alpha-1], out);
 figure
-%plot(1:1600, exponentialMA_data);
-dr=out;
+plot(1:1600, exponentialMA_data);
+dr=exponentialMA_data;
