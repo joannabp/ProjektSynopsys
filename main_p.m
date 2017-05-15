d304b05a7b7f80f6c8f056f44ba9fdf91bbb06f1
joannabp <joannabaranska@yahoo.com>
2017-05-15 18:54:03 +0200

    main_p z clk wypelnienie 1/2, update data_rec i drivera

--- main_p.m
+++ main_p.m
@@ -9,19 +9,22 @@ vector_length=1600;
 
 
 clk=[];
-
+clk_s=0;
 for i=1:1600
-    if (mod(i,31) ~= 0) 
-        clk(i)=0;
-    else 
-        clk(i)=1;
+    if (mod(i,16) == 0) 
+        clk_s=~clk_s;
+    %else 
+        
     end
+    clk(i)=clk_s;
 end
 sum_clk=sum(clk);
+
 input_num=randi([0 1], 100, 1);
 
 out_vect=driv_script(input_num);
 
+%clk=pll2(out_vect);
 [data, min_eye300_100, min_eye100_100, min_eye_100_300, setup, hold]=data_recovery(out_vect, clk);
 nums_str=[data{:}];
 
@@ -31,10 +34,12 @@ for i=1:50
     nums_split(i)=str2num(nums_str(i));
 end
 nums_split=nums_split';
-
+c=1;
 for i=1:50
     if nums_split(i)~=input_num(i) 
-        i
-        error='error'
+        i;
+        error='error';
+        c=c+1;
     end
+    
 end
