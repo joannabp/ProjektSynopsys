90077e026d8677c5e230a4b14abe872e8e4b96bc
blazkoo <blazko1994@o2.pl>
2017-05-28 19:46:25 +0200

    PLL+male zmiany
    Nowa wersja PLL zgodnie z zaleceniami plus odpowiadające zmiany w mainie i clk_gen - dodany clock idealny na początek, częstotliwość vco zmieniana tak aby jej dorównać.

--- clk_gen.m
+++ clk_gen.m
@@ -1,11 +1,10 @@
 function clk=clk_gen(t_clk,start)
 global vector_length;
 clk=zeros(1,vector_length);
-clk_out=zeros(1,vector_length);
+%start=randi(30,1,1);
 
 for i=start+1:vector_length
     if (mod(i-start,t_clk) <floor(t_clk/2)) 
         clk(i)=1; 						
     end
-end
 end
--- main.m
+++ main.m
@@ -5,11 +5,13 @@ global vector_length;
 
 vector_length=1600;
 input_data = randi([0 1], 100, 1);
-t_clk=randi(64,1,1)+1;
-start=randi(30,1,1);
-
-clk = clk_gen(t_clk,start);
-start=30;
+%t_clk=randi(64,1,1)+1;
+%start=randi(30,1,1);
+t_clk=31;
+start=31;
+clk_in = clk_gen(t_clk,start);
+%clk_tst=zeros(1,vector_length);
+data_out=zeros(1,vector_length);
 
 %---------------------driver----------------------------------------------%
 
@@ -17,12 +19,16 @@ driv_data = driv_script(input_data);
 
 %---------------------Channel---------------------------------------------%
 
-chanel_data = driv_data;
-szum=szum(driv_data,0.5);
+%channel_data = channel(driv_data,100);
+# for i=2:vector_length
+	# if(abs(driv_data(i)-driv_data(i-1))>=100)
+		# clk_tst(i)=1;
+	# end
+# end
 
 %---------------------Clock_Recovery--------------------------------------%
 
-clk_out=clock_recovery(clk,t_clk);
+clk_out=clock_recovery(clk_in,t_clk);
 
 %---------------------Data_Recovery---------------------------------------%
 
@@ -35,7 +41,7 @@ for i=1:length(nums_str)
     nums_split(i)=str2num(nums_str(i));
 end
 nums_split=nums_split';
-
+data_out=driv_script(nums_split);
 for i=1:50
     if nums_split(i)~=input_data(i) 
         printf('error w %d',i);
@@ -46,8 +52,11 @@ end
 plot(driv_data);
 ylabel("data in");
 figure
-plot(clk(1:66));
-ylabel('clk wzor');
+plot(clk_in);
+ylabel('clk in');
 figure
-plot(clk_out(1:66));
+plot(1:1600,clk_out*100,1:1600,data_out,1:1600,driv_data);
 ylabel('clk out');
+figure
+plot(data_out);
+ylabel('data out');
