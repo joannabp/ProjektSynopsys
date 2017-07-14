e15cc7e7266911e6b911567815b5a00da9d2df95
joannabp <joabaranska@outlook.com>
2017-07-14 16:16:15 +0200

    channel

--- channel.m
+++ channel.m
@@ -1,26 +1,44 @@
 function channel_data = channel(driv_data);
 global vector_length;
 
-d=100;
+format long
+d=7;
 Cp=34.06e-12;
 Rp=36.2e-3;
+Lp=5.06e-12;
 C=Cp*d;
 R=Rp*d;
+L=Lp*d;
 
 
 
 %syms s
 
-%h1=1/((R*C*s+1)*(R2*C2*s+1)*(R*C*s+1))
-w = logspace(0,20,128);
+w = logspace(0,20,32);
 % 
-% format long
-%  b=[1];
-%  a=[1 1/(R*C)];
+ b=[1];
+ a=[1  R*C*i*w]
+H = 1 ./ (1 + j*w*R*C); 
+ 
+subplot(2,1,1)
+semilogx(w,20*log10(abs(H)))
+title('Bode Plot for a very simple H(j\omega)');
+ylabel('Magnitude (dB)')
 
+% subplot(2,1,2)
+% semilogx(w, 180/pi * unwrap(angle(H)))
+% xlabel('Frequency (Hz)'), ylabel('Phase (degrees)')
+% hold on
+% 
+% H = L*j*w ./ (R + j*w*L);
+% subplot(2,1,1)
+% semilogx(w,20*log10(abs(H)))
+% title('Bode Plot for a very simple H(j\omega)');
+% ylabel('Magnitude (dB)')
 
-
-
+subplot(2,1,2)
+semilogx(w, 180/pi * unwrap(angle(H)))
+xlabel('Frequency (Hz)'), ylabel('Phase (degrees)')
 
 %c=tf(a, b);
 %H=freqs(b, a, w);
@@ -34,9 +52,9 @@ w = logspace(0,20,128);
 %channel_data=driv_data;
 
 % 
-Size =50; 
-b = (1/Size)*ones(1,Size);
-a = 1;
+% Size =50; 
+% b = (1/Size)*ones(1,Size);
+% a = 1;
 
 
 % H=(1/Size)*j*w ./ a;
@@ -49,19 +67,27 @@ a = 1;
 % subplot(2,1,2)
 % semilogx(w, 180/pi * unwrap(angle(H)))
 % xlabel('Frequency (Hz)'), ylabel('Phase (degrees)')
+%channel_data = filter(b, a, driv_data);
+
+Fs=1;
+% Default to allpass if invalid type is selected
+b = [ 1 0 ];
+a = [ 1 0 ];
+
+% Constants
+RC = R * C;
+T  = 1;
+
+% Analog Cutoff Fc
+w = 1 / (RC);
+
+% Prewarped coefficient for Bilinear transform
+a = exp(-2e-12/(RC))
+
+b=[1-a]
+a=[1 -a]
+
+    
+  
 channel_data = filter(b, a, driv_data);
 
-%  channel_data = filter(b,a,channel_data1);
-% channel_data3 = filter(b,a,channel_data2);
-% channel_data = filter(b,a,channel_data3);
-% 1 dla 65
-% 37
-% 124
-% 37
-% 1
-
-% 1 dla 40
-% 14
-% 172
-% 14
-% 1
