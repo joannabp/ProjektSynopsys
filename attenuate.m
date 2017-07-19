ee454ba147598e3a7fb18fc9eb372dc73af169e6
blazkoo <blazko1994@o2.pl>
2017-07-19 15:30:51 +0200

    clock generator
    Wywoluje sie funkcje clk_gen_f_not_id tak:
    clk=clk(f_clk,0,vector_length,0,t0,f0,liczba_okresow)
    gdzie:
    t0=[212,210,208,206,204,202,200,198,196,194,192,190];
    f0=zeros(1,12);
    for i=1:12
        f0(i)=200/t0(i)*freq;
    end
    liczba okresow jest dowolna, powinno skonczyc sie gdy dojdziemy do konca wektora, gdy podamy 5 a zmiesciloby sie 500 to po 5 beda same zera.

---
+++ attenuate.m
@@ -0,0 +1,45 @@
+function [curr_end, t_clk, f_clk, clk1]=attenuate(f_in,start,clk1,t0,f0,t)
+     f_diff=10^9;
+     j=0;
+     curr=start;
+     while((abs(f_diff)>5*10^7)&&(j<13))
+         j=j+1;
+         f_diff=f_in-f0(j);
+     end
+     t_clk=t0(j);
+     f_diff=f_diff/10^6;
+     clk1=clk1+f_diff/200;
+     step=t_clk;
+     curr_end=curr+step-1;
+     fprintf('roznica zamierzonej i faktycznej cz. to %f MHz\n',f_diff);
+     if(clk1>=0.5)
+         j=j+1;
+         t_clk=t0(j);
+         step=t_clk;%floor(T/EUI*freq/f0(j+1));
+         fprintf('akumulowana roznica zamierzonej i faktycznej cz. przekroczyla 50 MHz, przesuniecie okresu na %d, cz. na %f GHz, generacja sygnalu do %d zamiast %d\n',t_clk, f0(j)/10^9,curr+step,curr_end);
+         curr_end=curr+step;
+         f_diff=f_in-f0(j);
+         f_diff=f_diff/10^6;
+         if(t>1)
+            clk1=clk1+f_diff/200;%clk1-0.5;
+         else
+            clk1=f_diff/200;
+         end   
+         
+     elseif(clk1<0)
+         j=j-1;
+         t_clk=t0(j);
+         step=t_clk;%floor(T/EUI*freq/f0(j+1));
+         fprintf('akumulowana roznica zamierzonej i faktycznej cz. spadla ponizej 0, przesuniecie okresu na %d, cz. na %f GHz, generacja sygnalu do %d zamiast %d\n',t_clk, f0(j)/10^9,curr+step-2,curr_end);
+         curr_end=curr+step-2;
+         f_diff=f_in-f0(j);
+         f_diff=f_diff/10^6;
+         if(t>1)
+            clk1=clk1+f_diff/200;%clk1+0.5;
+         else
+            clk1=f_diff/200;
+         end
+     end
+     clk1
+     f_clk=f0(j)
+end
