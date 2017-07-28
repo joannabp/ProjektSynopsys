28cc5d9ce93d257b8f71b077f172cf5249ad5150
blazkoo <blazko1994@o2.pl>
2017-07-28 13:24:52 +0200

    Szybki clk generator
    Szybki clk generator, do drivera ustawic 'full' na 1, gdzie indziej 0.

--- clk_gen_f_not_id5.m
+++ clk_gen_f_not_id5.m
@@ -1,7 +1,9 @@
-function [clk,t_clk,f_clk,clk1,curr_end]=clk_gen_f_not_id5(f_in,start,stop,clk1,vector_length2)
+function [clk,t_clk,f_clk,clk1,curr_end]=clk_gen_f_not_id5(f_in,start,stop,clk1,vector_length2,full)
     global t0;
     global f0;
-    clk=zeros(1,stop-start);
+    if(full==1)
+        clk=zeros(1,stop-start);
+    end
     %t_clk=zeros(1,vector_length2);
     %f_clk=zeros(1,vector_length2);
     curr=1;
