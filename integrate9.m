495a665c0e205227484ab21d2722faae20dae5b8
blazkoo <blazko1994@o2.pl>
2017-07-28 09:23:52 +0200

    Update integrate9.m

--- integrate9.m
+++ integrate9.m
@@ -1,20 +1,8 @@
 function [v_int,v_int_ret,kp,z]=integrate9(v_df,v_int_prev,kp,ki,z)
   global acc_size;
   v_int=zeros(1,acc_size);
-  %ki=1;
-%  v_int_num=8192;
-%   v_df_num=0;
-%   change=1;
-%   if(v_df(1)==1)
-%       change=-1;
-%   end
-%   for i=2:v_df_size
-%       if(v_df(i)==1)
-%         v_df_num=v_df_num+change*2^(v_df_size-i);
-%       end
-%   end
   v_int_prev
-  v_int_num=v_int_prev+v_df*ki;%2^(acc_size-1)+v_df_prev+v_df
+  v_int_num=v_int_prev+v_df*ki;
   if(v_int_num>v_int_prev)
       z=z+1;
   elseif(v_int_num<v_int_prev)
@@ -22,11 +10,13 @@ function [v_int,v_int_ret,kp,z]=integrate9(v_df,v_int_prev,kp,ki,z)
   end
   if(z*(v_int_num-v_int_prev)<0&&kp>8)
       kp=8;
-  elseif(abs(z)>20)
+  elseif(abs(z)>5)
       if(kp<32)
           kp=kp+8;
-      else
+      elseif(kp<72)
           kp=kp+16;
+      else
+          kp=kp+24;
       end
       z=0;
   end
@@ -44,17 +34,10 @@ function [v_int,v_int_ret,kp,z]=integrate9(v_df,v_int_prev,kp,ki,z)
   elseif(v_int_num<0)
       v_int_num=0;
   end
-%   if(v_int_num>=0)
-%       v_int(1)=0;
-%   else
-%       v_int(1)=1;
-%   end
   v_int_num
   for i=1:acc_size
     if(v_int_num>=2^(acc_size-i))
       v_int(i)=1;
-      v_int_num=v_int_num-2^(acc_size-i);%v_int_num/abs(v_int_num)*2^(v_df_size-i);
+      v_int_num=v_int_num-2^(acc_size-i);
     end
   end
-  %v_int(2:v_df_size)=de2bi(bi2de(round(v_df_prev(2:v_df_size)))+change*bi2de(round(v_df(2:v_df_size))));
-  %v_int(2:v_df_size)=de2bi(bi2de(flipr(v_df_prev(2:v_df_size)))+change*bi2de(flipr(v_df(2:v_df_size))));
