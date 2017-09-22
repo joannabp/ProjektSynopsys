eaca453607fd1f149e7b2db6a2c73bcb922cb47e
joannabp <joabaranska@outlook.com>
2017-09-22 15:17:57 +0200

    Add files via upload

--- ctle_set.m
+++ ctle_set.m
@@ -29,45 +29,45 @@ switch cur_set
         %ctle=ctle(3, 2e9, 12); % (signal, fz, gain)
         fz=4.5e9;
         gain=12;
-        peak_val=120;
+        peak_val=130;
     case 3
         %ctle=ctle(3, 3.5e9, 12); % (signal, fz, gain)
         fz=3.5e9;
         gain=12;
-        peak_val=110;
+        peak_val=120;
     case 4
         fz=3e9;
         gain=12;
-        peak_val=100;
+        peak_val=110;
     case 5
         fz=2.5e9;
         gain=12;
-        peak_val=90;
+        peak_val=100;
     case 6
         fz=2e9;
         gain=12;
-        peak_val=80;
+        peak_val=90;
     case 7 % start set
         fz=1.5e9;
         gain=12;
-        peak_val=80;
+        peak_val=85;
     case 8
         fz=1.2e9;
         gain=12;
-        peak_val=75;
+        peak_val=80;
         
     case 9
         fz=1e9;
         gain=12;
-        peak_val=60;
+        peak_val=80;
     case 10
         fz=0.7e9;
         gain=12;
-        peak_val=60;
+        peak_val=75;
     case 11
         fz=0.4e9;
         gain=12;
-        peak_val=60;
+        peak_val=75;
 end
         
 %ctle=ctle(3, 1.2e9, 12); % (signal, fz, gain)
