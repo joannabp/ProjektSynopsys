6814a889da1621b691e1b9b1cfe23e45c1f66c8d
joannabp <joabaranska@outlook.com>
2017-10-25 07:41:14 +0200

    Add files via upload

--- setup_hold_check.m
+++ setup_hold_check.m
@@ -10,44 +10,7 @@ global unres_val;
 global UI_probes;
 
 vector_length=length(input_vector);
-% % ------------------- check setup time ------------% ---- 
-% s=1;
-% prev_val=input_vector(1);
-% setup(1)=1;
-% for i=2:vector_length
-%     c=(input_vector(i)-prev_val);
-%     if (c<abs(min_eye_opening))
-%         setup(s)=setup(s)+1;
-%     else 
-%         setup(s)=1;
-%     end
-%     if(rising_edge_detector(i)==1)
-%         s=s+1;
-%         %setup(s-1)=setup(s-1)-clk(i)-thresh-clk(i-1);
-%         setup(s)=1;
-%     end
-%     prev_val=input_vector(i);
-% end
-% % ------------------- check hold time ------------% ---- 
-% 
-% h=1;
-% prev_val=input_vector(1);
-% hold(1)=1;
-% for i=2:vector_length
-%     c=(input_vector(i)-prev_val);
-%     if(rising_edge_detector(i)==1)
-%         h=h+1;
-%         hold(h)=clk(i)-thresh-clk(i-1);
-%         prev_val=input_vector(i);
-%     end
-%     if (c<abs(min_eye_opening))
-%         
-%         hold(h)=hold(h)+1;
-%     end
-% end
-% 
-% setup=setup*T-setup_t;
-% hold=hold*T-hold_t;
+
 unres=unres_val;
 
 s_200=1;
@@ -104,40 +67,25 @@ for i=2:vector_length
     
     if(rising_edge_detector(i)==1)
         j=j+1;
-        
-         %setup(s-1)=setup(s-1)-clk(i)-thresh-clk(i-1);
-%          s_200=s_200+1;
-%         setup_200(s_200)=1;
-%          s200=s200+1;
-%         setup200(s200)=1;
-%         s0=s0+1;
-%         setup0(s0)=1;
-%         
-%         h_200=h_200+1;
-%         hold_200(h_200)=0.5;
-%         h0=h0+1;
-%         hold0(h0)=0.5;
-%         h200=h200+1;
-%         hold200(h200)=0.5;
     end
       
     % ------------------- check hold -200 time ------------% ---- 
       if(j==2)
 
-        if (abs(input_vector(i)-th_200(j))>min_eye_opening/2)
+        if (abs(input_vector(i)-th_200(1))>min_eye_opening/2)
             hold_200=hold_200+1;
         end
     % ------------------- check hold 0 time ------------% ---- 
 
 
-        if (abs(input_vector(i)-th0(j))>min_eye_opening/2)
+        if (abs(input_vector(i)-th0(1))>min_eye_opening/2)
             hold0=hold0+1;
 
         end
     % ------------------- check hold -200 time ------------% ----
 
 
-        if (abs(input_vector(i)-th200(j))>min_eye_opening/2)
+        if (abs(input_vector(i)-th200(1))>min_eye_opening/2)
             hold200=hold200+1;
         end
     end
@@ -161,12 +109,7 @@ k=1;
 
 for i=1:j 
     
-% if(setup200<0 || hold200<0)
-%           data(2)=1;
-%           data(1)=randi([0 1], 1, 1);
-%       else
-%           data(1:2)=prev_val;
-%       end
+
 
 
      if(setup200<0 || hold200<0 || setup0<0 || hold0<0 || setup_200<0 || hold_200<0)
@@ -182,9 +125,7 @@ for i=1:j
             end
 
          
-       %  data(1:2)=unres;
-       %data(1:2)=0;
-      %data(1:2)=prev_val;
+
      end
      k=k+2;
       %prev_val(1:2)=data(1:2);
